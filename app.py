0e721d83085b5c8a3f4ef15a72b3eba4536b1857
Spino <ashishyadav83022@gmail.com>
2026-08-31 19:23:40 +0530

    Create Flask website

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<h1>Hello! My Python website is working!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
